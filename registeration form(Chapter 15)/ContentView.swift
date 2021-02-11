fcf6fbf1b5578999eedb9fc7da0336535cef7f0b
Your mato <you@example.com>
2021-02-11 09:11:45 +0800

    Initial Commit

---
+++ registeration form(Chapter 15)/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  registeration form(Chapter 15)
+//
+//  Created by kittawat phuangsombat on 2021/2/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ registeration form(Chapter 15)/registeration_form_Chapter_15_App.swift
@@ -0,0 +1,17 @@
+//
+//  registeration_form_Chapter_15_App.swift
+//  registeration form(Chapter 15)
+//
+//  Created by kittawat phuangsombat on 2021/2/11.
+//
+
+import SwiftUI
+
+@main
+struct registeration_form_Chapter_15_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
